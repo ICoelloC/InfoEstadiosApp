a8483cb98233110c515aa9ecf80fff1017cbba3b
ICoelloC <ivancoellocortes@gmail.com>
2021-04-30 10:36:13 +0200

    Initial commit

---
+++ app/src/main/java/com/icoello/myapplication/LoginActivity.kt
@@ -0,0 +1,67 @@
+package com.icoello.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginEmail: EditText
+    private lateinit var loginPassword: EditText
+    private lateinit var loginButton: Button
+    private lateinit var loginGoRegisterButton: TextView
+
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        loginEmail = findViewById(R.id.loginEmail)
+        loginPassword = findViewById(R.id.loginPassword)
+        loginButton = findViewById(R.id.loginBtnLogin)
+        loginGoRegisterButton = findViewById(R.id.loginRegistrarseLink)
+
+        loginButton.setOnClickListener {
+            val email = loginEmail.text.toString()
+            val password = loginPassword.text.toString()
+            if (checkEmpty(email, password)) {
+                iniciarSesion(email, password)
+            }
+        }
+
+        loginGoRegisterButton.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+
+
+    }
+
+
+    private fun iniciarSesion(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(applicationContext, "Login failed!", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun checkEmpty(email: String, password: String): Boolean {
+        return email.isNotEmpty() && password.isNotEmpty()
+    }
+}
---
+++ app/src/main/java/com/icoello/myapplication/RegisterActivity.kt
@@ -0,0 +1,76 @@
+package com.icoello.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var registerEmail: EditText
+    private lateinit var registerPassword: EditText
+    private lateinit var registerRepeatPassword: EditText
+    private lateinit var registerButton: Button
+    private lateinit var registerGoLoginButton: TextView
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        auth = Firebase.auth
+
+        registerEmail = findViewById(R.id.registerEmail)
+        registerPassword = findViewById(R.id.registerPassword)
+        registerRepeatPassword = findViewById(R.id.registerRepitePassword)
+        registerButton = findViewById(R.id.registerBtnRegister)
+        registerGoLoginButton = findViewById(R.id.loginRegistrarseLink)
+
+        registerButton.setOnClickListener {
+            registrarseNormal()
+        }
+        registerGoLoginButton.setOnClickListener {
+            irLogin()
+        }
+
+
+    }
+
+    private fun irLogin() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+
+    private fun registrarseNormal() {
+
+        var email = registerEmail.text.toString()
+        var password = registerPassword.text.toString()
+        var repeatPassword = registerRepeatPassword.text.toString()
+
+        if (password.equals(repeatPassword) && checkEmpty(email, password, repeatPassword)) {
+            firebaseNormalRegister(email, password)
+        }
+
+    }
+
+    private fun firebaseNormalRegister(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(applicationContext, "Register failed!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun checkEmpty(email: String, password: String, repeatPassword: String): Boolean {
+        return email.isNotEmpty() && password.isNotEmpty() && repeatPassword.isNotEmpty()
+    }
+}
